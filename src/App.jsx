c24acc1d3e3c9a93d8ce8818ec4b1c8c668aefea
nishthaKataria <nishtha.kataria2004@gmail.com>
2025-08-28 18:58:44 +0530

    fix: add react-slick Slider import for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@
 
 
 import React, { useState, createContext, useContext } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import profilePic from './assets/profile.jpg'; // Replace with your own photo
 
 const ThemeContext = createContext();
