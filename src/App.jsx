684835ad49a46a142184d32123337340a675c3a3
nishthaKataria <nishtha.kataria2004@gmail.com>
2025-08-28 19:25:33 +0530

    Fix: remove duplicate imports and install react-slick and slick-carousel for project carousel

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ import React, { useState, createContext, useContext } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import profilePic from './assets/profile.jpg'; // Replace with your own photo
 
 const ThemeContext = createContext();
